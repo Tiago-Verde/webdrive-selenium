ad4603300213fdd185c4b4cfb1ecd689a528758f
Tiago-Verde <vutu.goes@gmail.com>
2021-06-11 21:59:02 -0300

    primeiro commit

---
+++ src/main/java/selenium/webdrive/App.java
@@ -0,0 +1,13 @@
+package selenium.webdrive;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
