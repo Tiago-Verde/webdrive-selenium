db6b1f3acac648e99e75cd3848e8df2ed0353dc9
Tiago-Verde <vutu.goes@gmail.com>
2021-06-22 21:29:54 -0300

    criação da classe TesteJavaScript()

---
+++ src/test/java/selenium/webdrive/TesteJavaScript.java
@@ -0,0 +1,39 @@
+package selenium.webdrive;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteJavaScript {
+
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+
+		submarino();
+
+	}
+
+	public static void submarino() {
+
+		System.setProperty("webdriver.chrome.driver", "C:/Tools/chromeDriver/chromedriver.exe");
+
+		driver = new ChromeDriver();
+
+		driver.get("https://www.submarino.com.br/");
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("document.getElementById('h_search-input').value='livros';");
+		js.executeScript("document.getElementById('h_search-btn').click();");
+
+		js.executeScript("window.scrollBy(0,1100)");
+		
+		WebElement livro = driver.findElement(By.cssSelector("img[class='src__Image-xr9q25-0 lkEXOf']"));  
+		
+		js.executeScript("arguments[0].click();", livro);
+
+	}
+
+}
