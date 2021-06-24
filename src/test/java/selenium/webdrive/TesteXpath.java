3c01671cedf43b20002bcd49ad0a84ab289db545
Tiago-Verde <vutu.goes@gmail.com>
2021-06-24 17:42:14 -0300

    alteração TesteXpath()

--- src/test/java/selenium/webdrive/TesteXpath.java
+++ src/test/java/selenium/webdrive/TesteXpath.java
@@ -1,5 +1,7 @@
 package selenium.webdrive;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,12 +21,16 @@ public class TesteXpath {
 		System.setProperty("webdriver.chrome.driver", "C:/Tools/chromeDriver/chromedriver.exe");
 
 		driver = new ChromeDriver();
+		
+		//uso de wait implicito
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 		driver.get("https://www.magazineluiza.com.br");
 		
 		driver.manage().window().maximize();
 		
 		driver.findElement(By.xpath("//input[@id='inpHeaderSearch']")).sendKeys("celular");
+		driver.findElement(By.xpath("//span[@title='Buscar']")).click();
 	}
 
 }
